0f89ea5e95a55dce98bfaa3efcc5e61d1d8e42ab
gyur2hwng <61445037+gyur2hwng@users.noreply.github.com>
2020-09-06 14:12:46 +0900

    Update boardmvcdb05.sql

--- boardmvcdb05.sql
+++ boardmvcdb05.sql
@@ -15,8 +15,4 @@ create table board(
     content varchar(20)
 );
 
-insert into board (num, writer, email, subject, password, reg_date, ref, re_step, re_level, readcount, content) 
-values (1, "황규리", "hi@gamil.com", "hi", "hi", now(), 1, 1, 1, 0, "hi");
-delete from board where num=1;
-
 select * from board;
